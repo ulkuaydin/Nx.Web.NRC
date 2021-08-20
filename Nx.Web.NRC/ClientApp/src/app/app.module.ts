fbdc9cb3a06151d328b00b84cafc75273f61ffcf
ulkuaydin <63735578+ulkuaydin@users.noreply.github.com>
2021-08-20 18:07:23 +0300

    Proje dosyası ekle.

---
+++ Nx.Web.NRC/ClientApp/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header.component';
+import { LandingPageComponent } from './views/landing-page/landing-page.component';
+import { BannerComponent } from './views/landing-page/banner.component';
+import { MobileComponent } from './views/landing-page/mobile.component';
+import { InfoComponent } from './views/landing-page/info.component';
+import { DashboardComponent } from './views/landing-page/dashboard.component';
+import { FooterComponent } from './shared/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LandingPageComponent,
+    BannerComponent,
+    MobileComponent,
+    InfoComponent,
+    DashboardComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
